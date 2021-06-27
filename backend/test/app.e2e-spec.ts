35e8599e0020efb6dd70cb0de148adb9f6ce4116
이재일 <radio941016@gmail.com>
2021-06-27 15:37:52 +0900

    before making whole App, now make app beforeEach

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -19,6 +19,31 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my movie API');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 200,
+          genres: ['test'],
+        });
+    });
+    it('Delete', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+      // methodNotAllowd : 405
+    });
+  });
+
+  describe('/movies/:id', () => {
+    it.todo('GET');
+    it.todo('DELETE');
+    it.todo('PATCH');
   });
 });
